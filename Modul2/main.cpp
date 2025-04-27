c853c8de56be4cf06ae2796c962d868521a9d323
areandra <muhammadariel2207@gmail.com>
2025-04-27 15:30:10 +0800

    commit

--- Modul2/main.cpp
+++ Modul2/main.cpp
@@ -1,35 +1,23 @@
 #include <iostream>
+
 using namespace std;
 
-struct Mahasiswa {
+struct dosen {
+    string nama[12] = {"Rebecca", "Carrot", "Ace", "Sabo", "Brook", "Zoro",
+                       "Nami", "Luffy", "Chopper", "Sanji", "Usop", "Robin"};
+};
+
+struct  mahasiswa {
     string nama;
-    string dosenWali;
-    string mataKuliah[3];
+    dosen dosenwali;
 };
 
 int main() {
-    Mahasiswa mhs;
+    mahasiswa mhs1;
+    mhs1.nama = "Irma";
 
-    cout << "Masukkan nama mahasiswa: ";
-    getline(cin, mhs.nama);
-
-    cout << "Masukkan nama dosen wali: ";
-    getline(cin, mhs.dosenWali);
-
-    cout << "Masukkan 3 mata kuliah yang diambil:\n";
-    for (int i = 0; i < 3 ; i++) {
-        cout << "- Mata kuliah ke-" << i+1 << ": ";
-        getline(cin, mhs.mataKuliah[i]);
-    }
-
-    cout << "\nData Mahasiswa\n"
-         << "Nama : " << mhs.nama << endl
-         << "Dosen Wali : " << mhs.dosenWali << endl
-         << "Mata Kuliah :\n";
-         
-    for (int i = 0; i < 3; i++) {
-        cout << "  " << i+1 << ". " << mhs.mataKuliah[i] << endl;
-    }
-
+    cout << "Nama : " << mhs1.nama << "\n";
+    cout << "Dosen Wali : " << mhs1.dosenwali.nama[8] << "\n";
+    
     return 0;
 }
--- Modul2/tugas/main.cpp
+++ Modul2/tugas/main.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
-
 using namespace std;
 
-struct dosen {
-    string nama[12] = {"Rebecca", "Carrot", "Ace", "Sabo", "Brook", "Zoro",
-                       "Nami", "Luffy", "Chopper", "Sanji", "Usop", "Robin"};
-};
-
-struct  mahasiswa {
+struct Mahasiswa {
     string nama;
-    dosen dosenwali;
-};
-
-struct matkul {
-    string nama;
-    dosen dosenPengamu[12];
-
+    string dosenWali;
+    string mataKuliah[3];
 };
 
 int main() {
-    
+    Mahasiswa mhs;
+
+    cout << "Masukkan nama mahasiswa: ";
+    getline(cin, mhs.nama);
+
+    cout << "Masukkan nama dosen wali: ";
+    getline(cin, mhs.dosenWali);
+
+    cout << "Masukkan 3 mata kuliah yang diambil:\n";
+    for (int i = 0; i < 3 ; i++) {
+        cout << "- Mata kuliah ke-" << i+1 << ": ";
+        getline(cin, mhs.mataKuliah[i]);
+    }
+
+    cout << "\nData Mahasiswa\n"
+         << "Nama : " << mhs.nama << endl
+         << "Dosen Wali : " << mhs.dosenWali << endl
+         << "Mata Kuliah :\n";
+         
+    for (int i = 0; i < 3; i++) {
+        cout << "  " << i+1 << ". " << mhs.mataKuliah[i] << endl;
+    }
+
+    return 0;
 }
